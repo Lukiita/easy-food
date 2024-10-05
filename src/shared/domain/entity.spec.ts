956633edc0d7140c060ef35338e30052f9884858
Lucas Fernandes Lima <lucas.liima030@gmail.com>
2024-10-05 19:57:08 -0300

    feat(shared/domain): add types and toJSON on BaseEntity

--- src/shared/domain/entity.spec.ts
+++ src/shared/domain/entity.spec.ts
@@ -1,7 +1,15 @@
 import { validate as uuidValidate } from 'uuid';
 import { BaseEntity } from './entity';
 
-class StubEntity extends BaseEntity { }
+class StubEntity extends BaseEntity {
+  toJSON() {
+    return {
+      id: this.id,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+    };
+  }
+}
 
 describe('BaseEntity Unit Test', () => {
   test('should create an entity with a valid UUID and timestamps', () => {
--- src/shared/domain/entity.ts
+++ src/shared/domain/entity.ts
@@ -1,18 +1,36 @@
 import { v4 as uuidv4 } from 'uuid';
+
 export type BaseEntityConstructorProps = {
   id?: string;
   createdAt?: Date;
   updatedAt?: Date;
 };
 
-export abstract class BaseEntity {
+export type BaseEntityRestoreProps = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type BaseEntityProps = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+export abstract class BaseEntity<T extends BaseEntityProps = any> {
   public readonly id: string;
   public readonly createdAt: Date;
-  public updatedAt: Date;
+  protected _updatedAt: Date;
 
   constructor(props: BaseEntityConstructorProps) {
     this.id = props.id || uuidv4();
     this.createdAt = props.createdAt || new Date();
-    this.updatedAt = props.updatedAt || new Date();
+    this._updatedAt = props.updatedAt || new Date();
   }
+
+  public get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  abstract toJSON(): T;
 }
